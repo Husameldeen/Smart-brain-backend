c3aa0361248b4650136338a83cb5c1b312bafbfc
Husameldeen <mhusam87@gmail.com>
2025-11-08 22:13:57 +0200

    edit data

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ import postgres from 'postgres';
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString : 'https://vwjnjvwretfvnjuiqska.supabase.co',
     user: 'postgres',
     password: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3am5qdndyZXRmdm5qdWlxc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MDk0NTMsImV4cCI6MjA3ODE4NTQ1M30.Jfe71yHobeakeQuMJVHEYiHkYJPF4db-RyevJD8Lumk',
     database: 'smartbraindata'
