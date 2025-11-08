b74740723047f1dc2c3809b20bda5002994a515a
Husameldeen <mhusam87@gmail.com>
2025-11-08 21:49:12 +0200

    edit data base

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ import handleSignin from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
 import handleImage from './controllers/image.js';
 import handleApi from './controllers/api.js';
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 const db = createClient('https://vwjnjvwretfvnjuiqska.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3am5qdndyZXRmdm5qdWlxc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MDk0NTMsImV4cCI6MjA3ODE4NTQ1M30.Jfe71yHobeakeQuMJVHEYiHkYJPF4db-RyevJD8Lumk')
+
+
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
 
 // const db = knex({
 //   client: 'pg',
