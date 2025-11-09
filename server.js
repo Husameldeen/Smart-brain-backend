b92c18aa952bc7349a9f99fc93c9cd56f557f629
Husameldeen <mhusam87@gmail.com>
2025-11-09 07:45:35 +0200

    good sign

--- server.js
+++ server.js
@@ -12,14 +12,7 @@ import { createClient } from '@supabase/supabase-js';
 import postgres from 'postgres';
 import pkg from 'pg';
 
-//Create a single supabase client for interacting with your database
-const supabase = createClient('https://vwjnjvwretfvnjuiqska.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3am5qdndyZXRmdm5qdWlxc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MDk0NTMsImV4cCI6MjA3ODE4NTQ1M30.Jfe71yHobeakeQuMJVHEYiHkYJPF4db-RyevJD8Lumk')
-//const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
-
-// const connectionString = 'https://vwjnjvwretfvnjuiqska.supabase.co'
-// const db = postgres(connectionString)
-
-//const db = knex(postgres(supabase));
+const db = createClient('https://vwjnjvwretfvnjuiqska.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3am5qdndyZXRmdm5qdWlxc2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MDk0NTMsImV4cCI6MjA3ODE4NTQ1M30.Jfe71yHobeakeQuMJVHEYiHkYJPF4db-RyevJD8Lumk')
 
 // const db = knex({ 
 //   connect to your own database here:
@@ -30,23 +23,11 @@ const supabase = createClient('https://vwjnjvwretfvnjuiqska.supabase.co', 'eyJhb
 //   }
 // });
 
-
-// const { Pool } = pkg;
-
-// const db = new Pool({
-//   host: 'https://vwjnjvwretfvnjuiqska.supabase.co',
-//   port: process.env.DB_PORT,
-//   user: 'mhusam87@gmail.com',
-//   password: process.env.DB_PASSWORD,
-//   database: smartbraindata,
-//   ssl: { rejectUnauthorized: false } // required for Supabase
-// });
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const { data, error } = await supabase
+const { data, error } = await db
   .from('users')
   .select('*')
 
