895ec19218cfc0379a5bb795f6122255f5f152e7
Husameldeen <mhusam87@gmail.com>
2025-11-09 20:49:53 +0200

    image.js modifie

--- controllers/image.js
+++ controllers/image.js
@@ -1,16 +1,20 @@
 
 const handleImage = (req, res, db) => {
     const { id } = req.body;
-    res.json(id)
     //console.log('id = ', id)
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then((entries) => {
-        //console.log('entries = ', entries[0])
-        res.json(entries[0])
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
+    // db('users').where('id', '=', id)
+    // .increment('entries', 1)
+    // .returning('entries')
+    // .then((entries) => {
+    //     //console.log('entries = ', entries[0])
+    //     res.json(entries[0])
+    // })
+    // .catch(err => res.status(400).json('unable to get entries'))
+
+    const { data, error } = db
+    .from('users')
+    .select('*')
+    res.json(data)
 }
 
 export default handleImage;
